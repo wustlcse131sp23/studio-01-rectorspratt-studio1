9a4017eacf6e0151bae0f5f0618dbca20d372a2d
rosiespratt <rosiespratt@localhost>
2023-01-26 12:12:41 -0600

    Finished HiOne and HiFour studio 1

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -10,8 +10,10 @@ public class HiOne {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
+		System.out.print(" Hi, Pat. How are you?" );
 		String name = in.nextLine();
+
+		
 		//
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
