b800d8277baa7df878832225091804dfca0da0fc
rosiespratt <rosiespratt@localhost>
2023-01-26 12:47:56 -0600

    Average studio 1

---
+++ src/studio1/Average.java
@@ -0,0 +1,20 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("The first vallue you want averaged?");
+	int n1 = in.nextInt();
+	System.out.println("The second value you want averaged?");
+	double n2 = in.nextInt();
+	System.out.println((n1 + n2) / 2);
+	
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
